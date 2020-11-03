3f15a3c64be733303f5ae68cccdc57f37932a637
Swanndolia <Swanndolia@gmail.com>
2020-11-03 03:15:43 +0100

    restart from scratch with base file of course

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const userRoutes = require("/routes/user");
-const saucesRoutes = require("/routes/sauces");
+const Thing = require("./models/Thing");
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
+const path = require("path");
 const app = express();
-app.use(bodyParser.json());
-app.use("/api/auth", userRoutes);
-app.use("/api/sauces", saucesRoutes);
-
 mongoose
-  .connect("mongodb+srv://Swanndolia:cdecdewsxT1@sopeckoko.gmam2.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+  .connect("mongodb+srv://Swanndolia:cdecdewsxT1@sopeckoko.gmam2.mongodb.net/sopeckoko?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connexion MongoDB ok !"))
-  .catch(() => console.log("Connexion MongoDB error !"));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  next();
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/stuff", (req, res, next) => {
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+  });
 });
+app.use(bodyParser.json());
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
